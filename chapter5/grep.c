a938c5ac1e76e892c02eb0fed009c146e711335f
yufanwei <w1229241853@gmail.com>
2021-08-01 20:10:35 +0100

    some fix

--- chapter5/grep.c
+++ chapter5/grep.c
@@ -11,6 +11,9 @@ int main(int argc, char* argv[]) {
     int number, except, found = 0;
 
     while (--argc > 0 && (*++argv)[0] == '-') 
+        // after the preminus operation, argc means the number of remaining arguments
+        // thus when it quits the case there is only one arguments which does not 
+        // begin with '-' is correct, otherwise the input is wrong
         while (ch = *++argv[0])
             switch (ch) {
             case 'x':
@@ -27,14 +30,15 @@ int main(int argc, char* argv[]) {
             }
     if (argc != 1)
         printf("usage: grep -x -n pattern\n");
-    
-    while (getLine(line, MAXLENGTH) != 0) {
-        lineno++;
-        if ((strstr(line, *argv) != NULL) != except) {
-            if (number)
-                printf("%d: ", lineno);
-            printf("%s", line);
-            found++;
+    else {
+        while (getLine(line, MAXLENGTH) != 0) {
+            lineno++;
+            if ((strstr(line, *argv) != NULL) != except) {
+                if (number)
+                    printf("%d: ", lineno);
+                printf("%s", line);
+                found++;
+            }
         }
     }
 
